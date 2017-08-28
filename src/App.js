8c5387349f4d79adc70b60762b2633f510838950
Julio Indriago <jcindriago@imthejungler.com>
2017-08-27 20:31:20 -0500

    refactor: GH-3: Creation SearchBook Component; Adding routing between components
    See: #3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import './App.css'
-import BookThumb from './BookThumb';
-import Bookshelf from './Bookshelf';
 import ListBooks from './ListBooks';
+import SearchBooks from './SearchBooks';
 
 class BooksApp extends Component {
   state = {
@@ -18,30 +18,12 @@ class BooksApp extends Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({showSearchPage: false})}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/* 
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
+        <Route path="/search" render={() => (
+          <SearchBooks/>
+        )}/>
+        <Route exact path="/" render={() => (
           <ListBooks/>
-        )}
+        )}/>
       </div>
     )
   }
